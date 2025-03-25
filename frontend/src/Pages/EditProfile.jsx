1c2a51ee3886216cca41eccf98081466d1973856
SwapnilSalunke1625 <salunkeswapnil145@gmail.com>
2025-03-25 17:45:01 +0530

    visual is done

--- frontend/src/Pages/EditProfile.jsx
+++ frontend/src/Pages/EditProfile.jsx
@@ -4,7 +4,8 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 import { FaTrash, FaCamera, FaSpinner } from 'react-icons/fa';
 import { toast } from 'react-toastify';
-import BackButton from '../components/BackButton';
+
+
 
 export default function EditProfile() {
     const { id } = useParams();
@@ -165,9 +166,10 @@ export default function EditProfile() {
 
     return (
         <>
-            <BackButton />
+        
             <div className="min-h-screen bg-gradient-to-b from-stdBg to-white py-8 px-4 sm:px-6 lg:px-8">
                 <div className="max-w-4xl mx-auto">
+                 
                     <div className="bg-white rounded-3xl shadow-2xl overflow-hidden transition-all duration-300 hover:shadow-xl">
                         {/* Enhanced Header */}
                         <div className="bg-gradient-to-r from-stdBlue to-color1 p-8 relative overflow-hidden">
